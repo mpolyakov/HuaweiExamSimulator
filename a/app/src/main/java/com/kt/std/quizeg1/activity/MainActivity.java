da344249545a1f949a725ee426ca89ebc832b982
mpolyakov <polyakov4@yandex.ru>
2020-04-21 12:42:45 +0300

    реализация списка тестов recyclerview

--- a/app/src/main/java/com/kt/std/quizeg1/activity/MainActivity.java
+++ a/app/src/main/java/com/kt/std/quizeg1/activity/MainActivity.java
@@ -5,10 +5,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.kt.std.quizeg1.R;
+import com.kt.std.quizeg1.adapters.CategoryAdapter;
+import com.kt.std.quizeg1.constants.AppConstants;
 import com.kt.std.quizeg1.models.quiz.CategoryModel;
 import com.kt.std.quizeg1.utilities.ActivityUtilities;
 import com.kt.std.quizeg1.utilities.AppUtilities;
@@ -23,38 +27,63 @@ import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class MainActivity extends BaseActivity {
+
+    private Activity activity;
+    private Context context;
+
     private Toolbar toolbar;
+
     private AccountHeader header = null;
     private Drawer drawer = null;
-    Activity activity;
-    Context context;
+
     private ArrayList<CategoryModel> categoryList;
+    private CategoryAdapter adapter = null;
+    private RecyclerView recyclerView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toolbar = findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         activity = MainActivity.this;
         context = getApplicationContext();
 
-        final IProfile profile = new ProfileDrawerItem().withIcon(R.drawable.sharp_star_black_18dp);
+
+
+        recyclerView = (RecyclerView) findViewById(R.id.rvContent);
+        recyclerView.setLayoutManager(new GridLayoutManager(activity, 2, GridLayoutManager.VERTICAL, false));
+
+        categoryList = new ArrayList<>();
+        adapter = new CategoryAdapter(context, activity, categoryList);
+        recyclerView.setAdapter(adapter);
+
+        initLoader();
+        loadData();
+
+        final IProfile profile = new ProfileDrawerItem().withIcon(R.drawable.ic_dev);
 
         header = new AccountHeaderBuilder()
                 .withActivity(this)
                 .withTranslucentStatusBar(true)
-                .withHeaderBackground(R.drawable.sharp_star_black_18dp)
+                .withHeaderBackground(R.drawable.header)
                 .withOnAccountHeaderProfileImageListener(new AccountHeader.OnAccountHeaderProfileImageListener() {
                     @Override
                     public boolean onProfileImageClick(View view, IProfile profile, boolean current) {
-                        ActivityUtilities.getInstance().invokeCustomUrlActivity(activity, CustomUrlActivity.class, getResources().getString(R.string.site),
-                                getResources().getString(R.string.site_url), false);
+                        ActivityUtilities.getInstance().invokeCustomUrlActivity(activity, CustomUrlActivity.class,
+                                getResources().getString(R.string.site), getResources().getString(R.string.site_url), false);
                         return false;
                     }
 
@@ -72,25 +101,28 @@ public class MainActivity extends BaseActivity {
                 .withHasStableIds(true)
                 .withAccountHeader(header)
                 .addDrawerItems(
-                        new PrimaryDrawerItem().withName("О приложении").withIcon(R.drawable.sharp_star_black_18dp).withIdentifier(10).withSelectable(false),
+                        new PrimaryDrawerItem().withName("О приложении").withIcon(R.drawable.ic_dev).withIdentifier(10).withSelectable(false),
 
-                        new SecondaryDrawerItem().withName("YouTube").withIcon(R.drawable.sharp_star_black_18dp).withIdentifier(20).withSelectable(false),
-                        new SecondaryDrawerItem().withName("Facebook").withIcon(R.drawable.sharp_star_black_18dp).withIdentifier(21).withSelectable(false),
-                        new SecondaryDrawerItem().withName("Twitter").withIcon(R.drawable.sharp_star_black_18dp).withIdentifier(22).withSelectable(false),
-                        new SecondaryDrawerItem().withName("Google+").withIcon(R.drawable.sharp_star_black_18dp).withIdentifier(23).withSelectable(false),
-
-                        new DividerDrawerItem(),
-                        new SecondaryDrawerItem().withName("Настройки").withIcon(R.drawable.sharp_star_black_18dp).withIdentifier(30).withSelectable(false),
-                        new SecondaryDrawerItem().withName("Оцените приложение").withIcon(R.drawable.sharp_star_black_18dp).withIdentifier(31).withSelectable(false),
-                        new SecondaryDrawerItem().withName("Поделиться").withIcon(R.drawable.sharp_star_black_18dp).withIdentifier(32).withSelectable(false),
-                        new SecondaryDrawerItem().withName("Соглашение").withIcon(R.drawable.sharp_star_black_18dp).withIdentifier(33).withSelectable(false),
+                        new SecondaryDrawerItem().withName("YouTube").withIcon(R.drawable.ic_youtube).withIdentifier(20).withSelectable(false),
+                        new SecondaryDrawerItem().withName("Facebook").withIcon(R.drawable.ic_facebook).withIdentifier(21).withSelectable(false),
+                        new SecondaryDrawerItem().withName("Twitter").withIcon(R.drawable.ic_twitter).withIdentifier(22).withSelectable(false),
+                        new SecondaryDrawerItem().withName("Google+").withIcon(R.drawable.ic_google_plus).withIdentifier(23).withSelectable(false),
 
                         new DividerDrawerItem(),
-                        new SecondaryDrawerItem().withName("Выход").withIcon(R.drawable.sharp_star_black_18dp).withIdentifier(40).withSelectable(false)
+                        new SecondaryDrawerItem().withName("Настройки").withIcon(R.drawable.ic_settings).withIdentifier(30).withSelectable(false),
+                        new SecondaryDrawerItem().withName("Оцените приложение").withIcon(R.drawable.ic_rating).withIdentifier(31).withSelectable(false),
+                        new SecondaryDrawerItem().withName("Поделитесь").withIcon(R.drawable.ic_share).withIdentifier(32).withSelectable(false),
+                        new SecondaryDrawerItem().withName("Соглашения").withIcon(R.drawable.ic_privacy_policy).withIdentifier(33).withSelectable(false),
+
+                        new DividerDrawerItem(),
+                        new SecondaryDrawerItem().withName("Выход").withIcon(R.drawable.ic_exit).withIdentifier(40).withSelectable(false)
+
+
                 )
                 .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
                     @Override
                     public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+
                         if (drawerItem != null) {
                             Intent intent = null;
                             if (drawerItem.getIdentifier() == 10) {
@@ -98,25 +130,26 @@ public class MainActivity extends BaseActivity {
 
                             } else if (drawerItem.getIdentifier() == 20) {
                                 AppUtilities.youtubeLink(activity);
-
                             } else if (drawerItem.getIdentifier() == 21) {
                                 AppUtilities.facebookLink(activity);
-
                             } else if (drawerItem.getIdentifier() == 22) {
                                 AppUtilities.twitterLink(activity);
                             } else if (drawerItem.getIdentifier() == 23) {
+                                AppUtilities.youtubeLink(activity);
                             } else if (drawerItem.getIdentifier() == 30) {
-//TODO invoke setting activity
+                                // TODO: invoke SettingActivity
                             } else if (drawerItem.getIdentifier() == 31) {
                                 AppUtilities.rateThisApp(activity);
                             } else if (drawerItem.getIdentifier() == 32) {
                                 AppUtilities.shareApp(activity);
                             } else if (drawerItem.getIdentifier() == 33) {
-                                ActivityUtilities.getInstance().invokeCustomUrlActivity(activity, CustomUrlActivity.class, getResources().getString(R.string.privacy), getResources().getString(R.string.privacy_url), false);
+                                ActivityUtilities.getInstance().invokeCustomUrlActivity(activity, CustomUrlActivity.class,
+                                        getResources().getString(R.string.privacy), getResources().getString(R.string.privacy_url), false);
                             } else if (drawerItem.getIdentifier() == 40) {
 
                             }
                         }
+
                         return false;
                     }
                 })
@@ -124,7 +157,6 @@ public class MainActivity extends BaseActivity {
                 .withShowDrawerOnFirstLaunch(true)
                 .withShowDrawerUntilDraggedOpened(true)
                 .build();
-
     }
 
     @Override
@@ -134,6 +166,52 @@ public class MainActivity extends BaseActivity {
         } else {
             AppUtilities.tapPromtToExit(this);
         }
+    }
+
+    private void loadData() {
+        showLoader();
+        loadJson();
+    }
+
+    private void loadJson() {
+        StringBuffer sb = new StringBuffer();
+        BufferedReader br = null;
+        try{
+            br = new BufferedReader(new InputStreamReader(getAssets().open(AppConstants.CONTENT_FILE)));
+            String temp;
+            while ((temp = br.readLine()) != null)
+                sb.append(temp);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                br.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        parseJson(sb.toString());
+    }
+
+    private void parseJson(String jsonData) {
+        try {
+            JSONObject jsonObject = new JSONObject(jsonData);
+            JSONArray jsonArray = jsonObject.getJSONArray(AppConstants.JSON_KEY_ITEMS);
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject object = jsonArray.getJSONObject(i);
+
+                String categoryId = object.getString(AppConstants.JSON_KEY_CATEGORY_ID);
+                String categoryName = object.getString(AppConstants.JSON_KEY_CATEGORY_NAME);
+
+                categoryList.add(new CategoryModel(categoryId, categoryName));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        hideLoader();
+        adapter.notifyDataSetChanged();
     }
 }
 
